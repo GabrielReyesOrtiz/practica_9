551524429bdafe3fbfa0647fc08b2e7515ab3f3a
GabrielReyesOrtiz <greyes17@ucol.mx>
2022-03-21 18:08:08 -0600

    Practica_9_Good

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
-//Aqui usamos el parametro GET en la ruta RAIZ
+
+/*//Aqui usamos el parametro GET en la ruta RAIZ
 app.get('/', (req, res)=> {
   //La función res.send() básicamente envía la respuesta HTTP.
   res.send('Hello World, this is the root route holaaaaaaaaaaa');
 });
 //La función app.listen() se usa para vincular y escuchar las conexiones en el host y puerto especificados.
-app.listen(3000);
-//Aqui usamos el parametro GET en la ruta UNO
-app.get('/uno', (req, res)=> {
-    //La función res.send() básicamente envía la respuesta HTTP.
-  res.send('Hello World, from route ONE');
+*/
+
+
+app.get('/prueba', (req, res) => { // route
+    res.send("Hello from prueba");
 });
+
+app.listen(3000);
+
+
