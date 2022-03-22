5a1cf830d8f224a928dd29e664bd48ba46eda2c1
GabrielReyesOrtiz <greyes17@ucol.mx>
2022-03-21 18:10:51 -0600

    Implemented Practica_9
    Signed-off-by: GabrielReyesOrtiz <greyes17@ucol.mx>

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ app.get('/', (req, res)=> {
   //La función res.send() básicamente envía la respuesta HTTP.
   res.send('Hello World, this is the root route holaaaaaaaaaaa');
 });
-//La función app.listen() se usa para vincular y escuchar las conexiones en el host y puerto especificados.
-*/
 
+app.get('/uno', (req, res)=> {
+  //La función res.send() básicamente envía la respuesta HTTP.
+  res.send('Hello World, from one');
+});
+*/
 
 app.get('/prueba', (req, res) => { // route
     res.send("Hello from prueba");
 });
-
+//La función app.listen() se usa para vincular y escuchar las conexiones en el host y puerto especificados.
 app.listen(3000);
 
 
